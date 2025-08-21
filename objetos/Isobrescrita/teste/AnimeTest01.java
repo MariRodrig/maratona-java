0c78a3976f7aff69dece96e81c696dd1d6e699ca
Mariana Rodrigues <marianar.adm@outlook.com>
2025-08-21 14:54:26 -0300

    feat: Sobrescrita de métodos

---
+++ objetos/Isobrescrita/teste/AnimeTest01.java
@@ -0,0 +1,10 @@
+package maratonaJava.objetos.Isobrescrita.teste;
+
+import maratonaJava.objetos.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Jacob");
+        System.out.println(anime);
+    }
+}
