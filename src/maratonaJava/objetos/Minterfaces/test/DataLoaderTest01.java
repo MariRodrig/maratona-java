7a7f4eb75e7816f6c238f026e7f84dfa9e2b3897
Mariana Rodrigues <marianar.adm@outlook.com>
2025-09-16 21:42:13 -0300

    feat: Interfaces

---
+++ src/maratonaJava/objetos/Minterfaces/test/DataLoaderTest01.java
@@ -0,0 +1,25 @@
+package maratonaJava.objetos.Minterfaces.test;
+
+
+import maratonaJava.objetos.Minterfaces.dominio.DataLoader;
+import maratonaJava.objetos.Minterfaces.dominio.DatabaseLoader;
+import maratonaJava.objetos.Minterfaces.dominio.FileLoader;
+
+public class DataLoaderTest01 {
+    public static void main(String[] args) {
+        DatabaseLoader databaseLoader = new DatabaseLoader();
+        FileLoader fileLoader = new FileLoader();
+        databaseLoader.load();
+        fileLoader.load();
+
+        databaseLoader.remove();
+        fileLoader.remove();
+
+        databaseLoader.checkPermission();
+        fileLoader.checkPermission();
+
+        DataLoader.retrieveMaxDataSize();
+        DatabaseLoader.retrieveMaxDataSize();
+    }
+}
+
